13c2b8dc1ff2975cd9a72bd8f1ff13311903841a
BoJavs <javiereric1309@gmail.com>
2023-10-16 08:34:15 -0600

    API

---
+++ source/routes/posts.ts
@@ -0,0 +1,21 @@
+/** source/routes/posts.ts */
+import express from 'express';
+import controller from '../controllers/posts';
+const router = express.Router();
+//User
+
+router.get('/user', controller.userLogin);
+router.get('/getPosts', controller.getPost);
+router.get('/getComments', controller.getComment);
+
+router.post('/user', controller.createUser);
+router.post('/post', controller.createPost);
+router.post('/comment', controller.createComment);
+
+router.patch('/like/:postId', controller.likePost);
+router.patch('/view/:postId', controller.viewPost);
+router.patch('/editPost/:postId',controller.editPost)
+router.patch('/report/:postId',controller.reportPost)
+
+
+export = router;
